d65c5abb875fbc08a460c14f6f7118b8d347a053
Lucie <lucie.geulin@eivp-paris.fr>
2020-11-17 10:27:01 +0100

    Update Occupation_ss_wk.py

--- Occupation_ss_wk.py
+++ Occupation_ss_wk.py
@@ -1,4 +1,5 @@
 #Puisqu'on a déterminé les week-end grâce au bruit, on peut enlever les week-ends des listes obtenues
+
 def horaire_semaine(L_lum,L_date,L_noise):
     hor=occupation_bu(L_lum,L_date)
     wk,sem=weekend(L_noise,L_date)          #On dispose des jours de week-end grâce au programme écrit précédemment
